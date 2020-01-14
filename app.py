aefaadbfbbd40a145ddff7edb8d2028639fb404b
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-01-13 21:10:14 -0500

    Create the first script

---
+++ app.py
@@ -0,0 +1,9 @@
+from pytrends.request import TrendReq
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from IPython.display import Image
+
+print("- - - - - - ISS-tracker - - - - - -")
+# TODO: All the app itself
