3d57222f76255f65ae2e6dfd5399785318c6d834
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-01-14 08:12:11 -0500

    Add request package and comment all packages above

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from pytrends.request import TrendReq
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib
-from IPython.display import Image
+# from pytrends.request import TrendReq
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib
+# from IPython.display import Image
+import requests
 
 print("- - - - - - ISS-tracker - - - - - -")
 # TODO: All the app itself
