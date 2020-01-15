30540ebfef2022c4958c35568c325c4020081cef
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-01-15 08:25:12 -0500

    Add while structure to get ISS position

--- app.py
+++ app.py
@@ -7,5 +7,15 @@
 import requests
 import time
 
+ISS_Position_API = "http://api.open-notify.org/iss-now.json"
+
 print("- - - - - - ISS-tracker - - - - - -")
-# TODO: All the app itself
+print("[          Version: 1.0.0          ]")
+
+while True:
+    print("Current position of ISS: ", end=' => ')
+    r = requests.get(url=ISS_Position_API)
+    space_station_location = (r.json())
+    print(f"Latitude {space_station_location['iss_position']['latitude']}", end=', ')
+    print(f"Longitude {space_station_location['iss_position']['longitude']}")
+    time.sleep(1)
