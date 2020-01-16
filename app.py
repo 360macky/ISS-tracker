72bebba5ee0943263943589f8a0b07035eaf2785
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-01-16 07:21:25 -0500

    Add colorama package

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-# from pytrends.request import TrendReq
-# import pandas as pd
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import matplotlib
-# from IPython.display import Image
+from colorama import Fore, Back, Style
 import requests
 import time
 
