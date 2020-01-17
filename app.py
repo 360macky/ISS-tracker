1ae3d6b3ff79ba5ff9459c39bfe84da4b46a2793
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-01-17 09:15:12 -0500

    Create an input to set the number of seconds to update the trace

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ print("\U0001f52d - - - - - - - - \U0001f6f0 ISS-tracker \U0001f4e1 - - - - - - 
 print(Fore.WHITE + "*                 Version 1.0.0                 *")
 print(Style.RESET_ALL, end='')
 
+seconds = 1
+seconds = int(input("* Type the number of seconds you want to wait to update the trace: "))
+
 while True:
     print("Current position of ISS: ", end=' => ')
     r = requests.get(url=ISS_Position_API)
     space_station_location = (r.json())
     print(f"Latitude {space_station_location['iss_position']['latitude']}", end=', ')
     print(f"Longitude {space_station_location['iss_position']['longitude']}")
-    time.sleep(1)
+    time.sleep(seconds)
